2243a62b0c228860b5b3f4a8f6a4c2e99785eb42
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-12 16:10:09 +0100

    pls

--- Dashies/javascript.js
+++ Dashies/javascript.js
@@ -4,15 +4,14 @@ $(document).ready(function (){
     });
 });
 
-var updatethething;
-function ReadFile(filepath){
-    var xhttp = new XMLHttpRequest;
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && (this.status == 200 || this.status == 0)){
+function ReadFile(filepath) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
             document.getElementById("main").innerHTML =
-            this.responseText();
+            this.responseText;
         }
-    }
+    };
     xhttp.open("GET", filepath, true);
     xhttp.send();
 }
