f798224aca888516ba6e462da3c13a9e98a8508b
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-12 15:47:33 +0100

    dash

---
+++ Dashies/javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+    $("#main").click(function(){
+        ReadFile("file://downloads.xml");
+    });
+});
+
+
+function ReadFile(filepath){
+    var xhttp = new XMLHttpRequest;
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && (this.status == 200 || this.status == 0)){
+            document.getElementById("main").innerHTML =
+            this.responseText();
+        }
+    }
+    xhttp.open("GET", "filepath", true);
+    xhttp.send();
+}
