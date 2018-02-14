e47dad09ff4014636905385c3f8ca3f0efec31dc
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-14 17:12:49 +0100

    Working Xml parsing download list

--- Dashies/javascript.js
+++ Dashies/javascript.js
@@ -1,34 +1,6 @@
 
-var xmlString =
-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
-
-"<download_list>"+
-    "<download>"+
-        "<name>generic payload</name>"+
-        "<url>"+
-            "<con>downloadsLocation/gey.txt</con>"+
-            "<gui>https://realwebsite.really/download/payloadgui.file</gui>"+
-        "</url>"+
-        "<desc>real amaze payload mkay</desc>"+
-    "</download>"+
-    "<download>"+
-        "<name>generic payload #2</name>"+
-        "<url>"+
-            "<con>https://realwebsite.really/download/payload2.file</con>"+
-            "<gui>https://realwebsite.really/download/payloadgui2.file</gui>"+
-        "</url>"+
-        "<desc>real amaze payload #2 mkay</desc>"+
-    "</download>"+
-"</download_list>";
-
 $(document).ready(function (){
-    $("#Load").click(function(){
-        LoadDownloads();
-    });
-    $("#Parse").click(function(){
-        parseXmlToList(xmlString);
-    });
-    parseXmlToList(xmlString);
+    LoadDownloads();
 });
 
 var downloads;
@@ -44,10 +16,7 @@ function LoadDownloads() {
 }
 
 
-
 function parseXmlToList(_xmlString){
-
-    console.log(_xmlString);
 
     parser = new DOMParser();
     xmlDoc = parser.parseFromString(_xmlString,"text/xml");
@@ -55,19 +24,19 @@ function parseXmlToList(_xmlString){
     var downloads = xmlDoc.getElementsByTagName("download");
 
     if(downloads.length <= 0) return;
-    else console.log(downloads.length);
     var htmlBuffer = "";
 
     for(var i = 0; i < downloads.length; i++){
-        htmlBuffer += "<br><div class='download'><span class='downloadName'>"
+        htmlBuffer += "<div class='download'><span class='downloadName'>"
         + downloads[i].childNodes[0].childNodes[0].nodeValue
-        + "</span><a href='"
-        + downloads[i].childNodes[1].childNodes[0].childNodes[0].nodeValue
-        + "' download>CON</a><a href='"
-        + downloads[i].childNodes[1].childNodes[1].childNodes[0].nodeValue
-        + "' download>GUI</a><div class='downloadDesc'>"
+        + "</span><div class='downloadDesc'>" 
         + downloads[i].childNodes[2].childNodes[0].nodeValue
-        + "</div></div>"
+        +"</div><a href='"
+        + downloads[i].childNodes[1].childNodes[0].childNodes[0].nodeValue
+        + "' download><button class='downloadButton'>CON</button></a><a href='"
+        + downloads[i].childNodes[1].childNodes[1].childNodes[0].nodeValue
+        + "' download><button class='downloadButton'>GUI</button></a>"
+        + "</div>"
     }
 
     $("#downloads").html(htmlBuffer);
