6ed1c13e9226a82989b6d9d333a44f937dfbc76a
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-14 13:26:35 +0100

    poof

--- Dashies/javascript.js
+++ Dashies/javascript.js
@@ -1,36 +1,70 @@
+
+var xmlString = 
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+
+"<download_list>"+
+    "<download>"+
+        "<name>generic payload</name>"+
+        "<url>"+
+            "<con>downloadsLocation/gey.txt</con>"+
+            "<gui>https://realwebsite.really/download/payloadgui.file</gui>"+
+        "</url>"+
+        "<desc>real amaze payload mkay</desc>"+
+    "</download>"+
+    "<download>"+
+        "<name>generic payload #2</name>"+
+        "<url>"+
+            "<con>https://realwebsite.really/download/payload2.file</con>"+
+            "<gui>https://realwebsite.really/download/payloadgui2.file</gui>"+
+        "</url>"+
+        "<desc>real amaze payload #2 mkay</desc>"+
+    "</download>"+
+"</download_list>";
+
 $(document).ready(function (){
     $("#Load").click(function(){
         LoadDownloads();
     });    
     $("#Parse").click(function(){
-        parseXmlToList(Text);
+        parseXmlToList(xmlString);
     });
+    parseXmlToList(xmlString);
 });
-
-var text;
 
 function LoadDownloads() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            text = xhttp.responseText;
-            console.log(text);
+            xmlString = xhttp.responseText;
+            console.log(xmlString);
         }
     };
     xhttp.open("GET", "downloads.xml", true);
-    xhttp.send();    
+    xhttp.send();
 }
 
-function parseXmlToList(xmlText){
-    var parser = new DOMParser;
-    var downloadList = parser.parseFromString(text, "text/xml").getElementsByTagName("download");
+function parseXmlToList(_xmlString){
+    console.log(_xmlString);
 
-    var outputHtml;
-    for(var i = 0; i < downloadList.length; i++) {
-        var download;
-        outputHtml = downloadList[i].childNodes[0].nodeValue;
-        console.log(outputHtml);
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(_xmlString,"text/xml");
+
+    var downloads = xmlDoc.getElementsByTagName("download");
+
+    if(downloads.length <= 0) return;
+    var htmlBuffer = "";
+
+    for(var i = 0; i < downloads.length; i++){
+        htmlBuffer += "<br><div class='download'><span class='downloadName'>"
+        + xmlDoc.getElementsByTagName("download")[i].childNodes[0].childNodes[0].nodeValue
+        + "</span><a href='"
+        + xmlDoc.getElementsByTagName("download")[i].childNodes[1].childNodes[0].childNodes[0].nodeValue
+        + "' download>CON</a><a href='"
+        + xmlDoc.getElementsByTagName("download")[i].childNodes[1].childNodes[1].childNodes[0].nodeValue
+        + "' download>GUI</a><div class='downloadDesc'>"
+        + xmlDoc.getElementsByTagName("download")[i].childNodes[2].childNodes[0].nodeValue
+        + "</div></div>"
     }
 
-    console.log("Final: " + outputHtml);
+    document.getElementById("downloads").innerHTML = htmlBuffer;
 }
