4033ba9bfc1a7c8ba64d0b7cfdf61fecefd59124
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-14 20:30:37 +0100

    replaced xml with json

--- Dashies/javascript.js
+++ Dashies/javascript.js
@@ -1,45 +1,38 @@
 
-
 $(document).ready(function (){
-    parseXmlToList(xmlString);
+    LoadFromJson();
 });
 
-var downloads;
-function LoadDownloads() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+function LoadFromJson() {
+
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", "downloads.json", true);
+    rawFile.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            parseXmlToList(xhttp.responseText);
+            var downloads = JSON.parse(rawFile.responseText);
+            console.log(downloads[0].name);
+        
+            var htmlBuffer = "";
+        
+            for(var i = 0; i < downloads.length; i++){
+                htmlBuffer += "<div class='download'><span class='downloadName'>"
+                + downloads[i].name
+                + "</span><div class='downloadDesc'>" 
+                + downloads[i].desc
+                +"</div><a href='"
+                + downloads[i].urlConsole
+                + "' download><button class='downloadButton'>CON</button></a><a href='"
+                + downloads[i].urlGUI
+                + "' download><button class='downloadButton'>GUI</button></a>"
+                + "</div>"
+            }
+        
+            $("#downloads").html(htmlBuffer);
+         
         }
     };
-    xhttp.open("GET", "downloads.xml", true);
-    xhttp.send();
-}
-
-function parseXmlToList(_xmlString){
-
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(_xmlString,"text/xml");
-
-    var downloads = xmlDoc.getElementsByTagName("download");
-
-    if(downloads.length <= 0) return;
-    var htmlBuffer = "";
-
-    for(var i = 0; i < downloads.length; i++){
-        htmlBuffer += "<div class='download'><span class='downloadName'>"
-        + downloads[i].childNodes[0].childNodes[0].nodeValue
-        + "</span><div class='downloadDesc'>" 
-        + downloads[i].childNodes[2].childNodes[0].nodeValue
-        +"</div><a href='"
-        + downloads[i].childNodes[1].childNodes[0].childNodes[0].nodeValue
-        + "' download><button class='downloadButton'>CON</button></a><a href='"
-        + downloads[i].childNodes[1].childNodes[1].childNodes[0].nodeValue
-        + "' download><button class='downloadButton'>GUI</button></a>"
-        + "</div>"
-    }
-
-    $("#downloads").html(htmlBuffer);
+    rawFile.send(null);
 }
 
 
