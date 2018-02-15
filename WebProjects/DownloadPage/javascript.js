baa3442ae1060b1dd07451815789494ab389d1f0
JustTheCoolDude <JustTheCoolDude@gmail.com>
2018-02-15 09:55:40 +0100

    fup

--- WebProjects/DownloadPage/javascript.js
+++ WebProjects/DownloadPage/javascript.js
@@ -25,7 +25,7 @@ function LoadFromJson() {
             var htmlBuffer = "";
         
             for(var i = 0; i < downloads.length; i++){
-                htmlBuffer += `<div class='download'><span class='downloadName'>${downloads[i].name}</span><div class='downloadDesc'>${downloads[i].desc}</div>${downloads[i].ConsoleFileName == "" ? "" : "<a href='${downloadLocation + downloads[i].ConsoleFileName"}' download><button class='downloadButton'>CON</button></a>}${downloads[i].GUIFileName == "" ? "" : "<a href='${downloadLocation + downloads[i].GUIFileName}' download><button class='downloadButton'>GUI</button></a>"}</div>`;
+                htmlBuffer += `<div class='download'><span class='downloadName'>${downloads[i].name}</span><div class='downloadDesc'>${downloads[i].desc}</div>${downloads[i].ConsoleFileName == "" ? "" : `<a href='${downloadLocation + downloads[i].ConsoleFileName}' download><button class='downloadButton'>CON</button></a>`}${downloads[i].GUIFileName == "" ? "" : `<a href='${downloadLocation + downloads[i].GUIFileName}' download><button class='downloadButton'>GUI</button></a>`}</div>`;
             }
         
             $("#downloads").html(htmlBuffer);
